4edafe81949bff41e578edaf71ae405c53dabd4c
31031129 <31031129@089781-P33S09.macklabs.br>
2013-05-07 20:58:49 -0300

    Adicionada classe teste para branch

---
+++ AutoComp/src/autocomp/TesteJDBC.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package autocomp;
+
+/**
+ *
+ * @author 31031129
+ */
+public class TesteJDBC {
+    public static void main(String[] args){
+        System.out.println("Ardriano viado");
+    }
+}
