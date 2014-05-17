a740d396cf8d35ab3df55740395baf4c539109a7
Douglas <Douglas@192.168.1.2>
2014-05-17 19:49:06 -0300

    Adição das novas classes de teste baseadas no documento de teste.

---
+++ core/test/CaseTest/CT06.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package CaseTest;
+
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ *
+ * @author Douglas
+ */
+@RunWith(PowerMockRunner.class)
+public class CT06 {
+    
+}
---
+++ core/test/CaseTest/CT10.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package CaseTest;
+
+import autocomp.controller.CursosController;
+import autocomp.controller.DisciplinaController;
+import autocomp.controller.UsuarioController;
+import autocomp.dao.AlunoDAO;
+import autocomp.dao.UsuarioDAO;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ *
+ * @author Douglas
+ */
+@RunWith(PowerMockRunner.class)
+public class CT10 {
+
+    UsuarioController usuarioControllerMock;
+    UsuarioDAO usuarioDAOMock;     
+    
+    @Before
+    public void setup() throws Exception {
+        usuarioDAOMock = PowerMock.createMock(UsuarioDAO.class);
+        PowerMock.expectNew(UsuarioDAO.class).andReturn(usuarioDAOMock);        
+        
+        usuarioControllerMock = PowerMock.createMock(UsuarioController.class);
+        PowerMock.expectNew(UsuarioController.class).andReturn(usuarioControllerMock);
+    }
+    
+    
+}
