636701c31da93a07458342407716df681ec36c6d
Douglas <Douglas@192.168.1.2>
2014-05-17 21:03:03 -0300

    Adição dos CT 01 02 03

--- core/test/CaseTest/CT01.java
+++ core/test/CaseTest/CT01.java
@@ -3,17 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package CaseTest;
 
-import autocomp.controller.CursosController;
-import autocomp.controller.DisciplinaController;
 import autocomp.controller.UsuarioController;
-import autocomp.dao.AlunoDAO;
 import autocomp.dao.UsuarioDAO;
+import autocomp.model.Grupo;
+import autocomp.model.Usuario;
+import org.easymock.EasyMock;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.easymock.PowerMock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
@@ -21,19 +23,43 @@ import org.powermock.modules.junit4.PowerMockRunner;
  * @author Douglas
  */
 @RunWith(PowerMockRunner.class)
+@PrepareForTest({UsuarioController.class})
 public class CT01 {
 
     UsuarioController usuarioControllerMock;
-    UsuarioDAO usuarioDAOMock;     
-    
+    UsuarioDAO usuarioDAOMock;
+    String TIA;
+    String Senha;
+
     @Before
     public void setup() throws Exception {
         usuarioDAOMock = PowerMock.createMock(UsuarioDAO.class);
-        PowerMock.expectNew(UsuarioDAO.class).andReturn(usuarioDAOMock);        
-        
+        PowerMock.expectNew(UsuarioDAO.class).andReturn(usuarioDAOMock);
+
         usuarioControllerMock = PowerMock.createMock(UsuarioController.class);
         PowerMock.expectNew(UsuarioController.class).andReturn(usuarioControllerMock);
+        
+        TIA = "12345678";
+        Senha = "123";
     }
-    
-    
+
+    public void applyMockReplay() {
+        PowerMock.replay(usuarioDAOMock, UsuarioDAO.class);
+        PowerMock.replay(usuarioControllerMock, UsuarioDAO.class);
+    }
+
+    @Test
+    public void CSU01FP_01() {
+
+        Usuario usuario = new Usuario(TIA, Senha, "nome",Grupo.ADMINISTRADOR);
+
+        EasyMock.expect(usuarioControllerMock.autenticar(TIA, Senha)).andReturn(usuario);
+        applyMockReplay();
+        
+        Usuario result = usuarioControllerMock.autenticar(TIA, Senha);
+        
+        Assert.assertEquals(usuario, result);
+
+    }
+
 }
--- core/test/CaseTest/CT03.java
+++ core/test/CaseTest/CT03.java
@@ -6,7 +6,19 @@
 
 package CaseTest;
 
+import autocomp.controller.QuestoesController;
+import autocomp.controller.UsuarioController;
+import autocomp.dao.QuestaoDAO;
+import autocomp.dao.UsuarioDAO;
+import autocomp.model.Questao;
+import org.easymock.EasyMock;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
@@ -14,6 +26,33 @@ import org.powermock.modules.junit4.PowerMockRunner;
  * @author Douglas
  */
 @RunWith(PowerMockRunner.class)
+@PrepareForTest({QuestoesController.class})
 public class CT03 {
     
+    QuestaoDAO questaoDaoMock;
+    
+    @Before
+    public void setup() throws Exception {
+        questaoDaoMock = PowerMock.createMock(QuestaoDAO.class);
+        PowerMock.expectNew(QuestaoDAO.class).andReturn(questaoDaoMock);
+    }
+
+    public void applyMockReplay() {
+        PowerMock.replay(questaoDaoMock, UsuarioDAO.class);
+    }
+    
+    @Test
+    public void CSU03FP_01()
+    {
+        //Preparação
+        int id = 1234;
+        Questao questao = new Questao(null, null, null, null, null, null, id, Questao.QuestaoDificuldade.FACIL, null, null);
+        EasyMock.expect(questaoDaoMock.getById(id)).andReturn(questao);
+        PowerMock.replay(questaoDaoMock,QuestaoDAO.class);
+        //Execução
+        QuestoesController questoes = new QuestoesController();
+        Questao result = questoes.getQuestao(id);
+        //Check
+        assertEquals(questao,result);
+    }
 }
